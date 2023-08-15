6c9e5f62ab642b7fa38d185dd0336540fed98a78
huangwu1111 <1243930473@qq.com>
2023-08-15 10:45:15 +0800

    sdsd
    sdsd

---
+++ Example/Pods/Target Support Files/Pods-PublishTest1_Example/Pods-PublishTest1_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PublishTest1_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PublishTest1_Example
+@end
---
+++ Example/Pods/Target Support Files/PublishTest1/PublishTest1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PublishTest1 : NSObject
+@end
+@implementation PodsDummy_PublishTest1
+@end
